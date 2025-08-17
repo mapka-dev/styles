2f1f517cbda475601049d747c231963821f0a795
chyzwar <schizek.marcin@gmail.com>
2025-08-17 12:52:49 +0200

    feat(): update styles with new schema, added linting, small fixes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Styles
 
-This is collection of styles for mapbox-gl and maplibre-gl.
+This is collection of styles for mapbox-gl and maplibre-gl styles.
+These are either opensource styles from [openmaptiles](https://github.com/openmaptiles/openmaptiles) or [maputnik](https://github.com/maputnik/editor). or custom styles created by us.
--- bin/format.ts
+++ bin/format.ts
@@ -1,8 +1,8 @@
-import { format } from '@maplibre/maplibre-gl-style-spec'
-import { readFileSync, writeFileSync } from 'node:fs';
+import { readFileSync, writeFileSync } from "node:fs";
+import { format } from "@maplibre/maplibre-gl-style-spec";
 
-const STYLE_JSON = './style.json'
-const ENCODING = 'utf8';
+const STYLE_JSON = "./style.json";
+const ENCODING = "utf8";
 
 const style = readFileSync(STYLE_JSON, ENCODING);
 const parsed = JSON.parse(style);
